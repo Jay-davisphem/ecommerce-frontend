910c0fc40c2e0f4d58572e9667a56440d7ecf654
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-04-14 15:44:44 +0100

    add contents to cart

--- components/EmptyCart.tsx
+++ components/EmptyCart.tsx
@@ -1,7 +1,18 @@
+import img from "../assets/assets_images/emptyCart.svg";
 function EmptyCart() {
   return (
     <>
-      <h1>Empty</h1>
+      <div>
+        <img src={img} />
+        <h2>Empty Basket</h2>
+        <p className="text-grey text-xs">
+          You basket is still empty, browser the delicious and mouth watering
+          menu from the bardeal
+        </p>
+        <button className="block border bg-main text-white rounded rounded-md">
+          Shopping Now
+        </button>
+      </div>
     </>
   );
 }
