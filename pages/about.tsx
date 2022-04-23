81cb42f73283ff285d835a717407f8e87d678372
David Oluwafemi Joshua <davidoluwafemi178@gmail.com>
2022-04-23 01:01:26 +0100

    rm pdf module

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,6 @@
-import pdfFile from "../public/backend_dev_oluwafemi.pdf";
 import AboutMe from "../components/AboutMe";
 
 export default function About() {
-  const pdfUrl = 'file:///home/davisphem/Documents/Programs/next-apps/ecommerce-frontend/public/backend_dev_oluwafemi.pdf'
+  const pdfUrl = 'https://github.com/jay-davisphem/ecormerce-frontend/public/backend_dev_oluwafemi.pdf'
   return <AboutMe pdfFile={pdfUrl} />;
 }
